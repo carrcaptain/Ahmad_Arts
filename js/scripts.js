c73f74f10c19e4c464077ef95af0de6f95cc0c4e
Cameron Carr <carrcaptainpatna@gmail.com>
2021-07-19 08:24:33 -0400

    Animation Home

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+var modal = document.getElementById('myModal');
+
+var img = document.getElementById('myImg');
+img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  modal.style.display = "none";
+}
